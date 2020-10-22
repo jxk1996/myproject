3743fc3c90b4ed947edbb9923d7408280b134d36
jxk <1712203023@qq.com>
2020-10-22 11:34:08 +0800

    test jenkins

---
+++ UserService/src/main/java/com/jxk/userservice/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.jxk.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String helloToMyProject(String name){
+        return "hello"+name;
+    }
+}
